762e6d549023edaefc7123805c97346608b09871
HessaAlRahmani <76619150+HessaAlRahmani@users.noreply.github.com>
2021-01-05 20:56:06 +0300

    Done

--- question1.js
+++ question1.js
@@ -1,1 +1,27 @@
 // قم بحل السؤال الأول هنا
+let pets = [
+    'Ghanima','Haifaa','Lulu','Fairouz','Fishy','Cinna'
+];
+
+console.log(pets)
+
+console.log(pets.pop())
+
+console.log(pets)
+
+pets.push('Pickle')
+
+console.log(pets)
+
+pets[2]='Coco'
+
+console.log(pets)
+
+let Ghanima = {
+    name: 'Ghanoom',
+    age: '5 human years, 36 cat years',
+    breed: 'Siamease'
+}
+
+Ghanima.emoji='🐈'
+console.log(Ghanima.age)
